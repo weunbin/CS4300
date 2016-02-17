28cd4bc7fa2ee4b53236ceb7e009aed0f9f98099
seyun316 <seyun316@uga.edu>
2016-02-17 06:35:27 -0500

    Update script.js

--- Project3/script.js
+++ Project3/script.js
@@ -7,3 +7,12 @@ function capitalLast() {
     var input = document.getElementById("lname");
     input.value = input.value.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
+
+function phone() {
+    var input = document.getElementById("telephone");
+    var regex = new RegExp("(\D\d{3}\D\d{3}\D\d{4})");
+    var regex2 = new RegExp("(\d{3}\D\d{3}\D\d{4})");
+    if (!regex.test(input.value) && !regex2.test(input.value)) {
+        alert("Incorrect Format: <something>ddd<something>ddd<something>dddd");
+    }
+}

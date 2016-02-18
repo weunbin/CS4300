65dd6428e1fb0abfdadedb78389b95fa2b5cb3f6
weunbin <weunbin@uga.edu>
2016-02-18 18:11:30 -0500

    done
    done

--- Project3/script.js
+++ Project3/script.js
@@ -1,7 +1,7 @@
 
 
 function capitalFirst() {
-    var input = document.getElementById("fname");
+    var input = document.getElementById('fname');
     input.value = input.value.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
@@ -20,18 +20,17 @@ function phone() {
     }
 }
 
-
-function autoComplete(){
-    var textArray = [];
+function alertCity(){
+        var textArray = [];
     var cityArray = [];
     var zipcodeArray = [];
     var tempArray = [];
-    var filePath = "\zipcodes.csv";
+    var filePath = "zipcodes.csv";
     var zipCodeInc = 0;
-    var text;
+    var text = '';
     
     var csvFile = new XMLHttpRequest();
-    csvFile.open("GET", filePath, false);
+    csvFile.open("GET", filePath,false);
     csvFile.onreadystatechange = function ()
     {
         if(csvFile.readyState === 4)
@@ -40,30 +39,80 @@ function autoComplete(){
             {
                 text = csvFile.responseText;
                 textArray = text.split("\n");
-                
+                for(i = 0; i < textArray.length; i++){
+                    for(j = 0; j < textArray[i].length; i++){
+                        tempArray = textArray[i].split(",");
+                        cityArray[i] = tempArray[0];
+                    }
+                }
+            }
+        }
+    }
+    
+    csvFile.send(null);
+    var notFound = true;
+    var citySelected = document.getElementById('city');
+    var selectedCity = citySelected.value;
+    for(var q = 0; q < cityArray.length; q++){
+            if(selectedCity == cityArray[q])
+                notFound = false;
+    }      
+    if(notFound)
+        alert(selectedCity + ' does not exit!');
+}
+
+
+function autoComplete(){
+    var textArray = [];
+    var cityArray = [];
+    var zipcodeArray = [];
+    var tempArray = [];
+    var filePath = "zipcodes.csv";
+    var zipCodeInc = 0;
+    var text = '';
+    
+    var csvFile = new XMLHttpRequest();
+    csvFile.open("GET", filePath,false);
+    csvFile.onreadystatechange = function ()
+    {
+        if(csvFile.readyState === 4)
+        {
+            if(csvFile.status === 200 || csvFile.status == 0)
+            {
+                text = csvFile.responseText;
+                textArray = text.split("\n");
                 for(i = 0; i < textArray.length; i++){
                     for(j = 0; j < textArray[i].length; i++){
                         tempArray = textArray[i].split(",");
                         cityArray[i] = tempArray[0];
                         var zipcodeLength = tempArray.length - 1;
+                        zipcodeArray[i] = new Array(zipcodeLength);
                         for(k = 0; k < zipcodeLength; k++){
-                            zipcodeArray[i] = tempArray[k+1];
+                            zipcodeArray[i][k] = tempArray[k+1];
+                            zipcodeArray[i][k] = zipcodeArray[i][k].replace(/[\r\n]/,'');
                         }
                     }
                 }
             }
         }
     }
+    
     csvFile.send(null);
     var options = '';
     for(var o = 0; o < cityArray.length; o++){
-        options += '<option value="'+cityArray[o]+'"/>';
+        options += '<option value="'+cityArray[o]+'">';
     }
     document.getElementById('cities').innerHTML = options;
-    
+    var citySelected = document.getElementById('city');
+    var selectedCity = citySelected.value;
     var zipCodeoptions = '';
-    for(var m = 0; m < zipcodeArray.length; m++){
-        zipCodeoptions += '<option value="'+zipcodeArray[m]+'"/>';
+    var getCity = 0;
+    for(var q = 0; q < cityArray.length; q++){
+        if(selectedCity == cityArray[q])
+            getCity = q;
+    }
+    for(var n = 0; n < zipcodeArray[getCity].length; n++){
+            zipCodeoptions += '<option value="'+zipcodeArray[getCity][n]+'">';
     }
     document.getElementById('zipcodes').innerHTML = zipCodeoptions;
     

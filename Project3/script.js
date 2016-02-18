ebf81b33098d6c9e8f5c14089efd09f961b1c6eb
weunbin <weunbin@uga.edu>
2016-02-18 18:43:07 -0500

    commit
    commit

--- Project3/script.js
+++ Project3/script.js
@@ -20,8 +20,60 @@ function phone() {
     }
 }
 
+function autoCompleteZipcode(){
+       var textArray = [];
+    var cityArray = [];
+    var zipcodeArray = [];
+    var tempArray = [];
+    var filePath = "zipcodes.csv";
+    var zipCodeInc = 0;
+    var text = '';
+    
+    var csvFile = new XMLHttpRequest();
+    csvFile.open("GET", filePath,false);
+    csvFile.onreadystatechange = function ()
+    {
+        if(csvFile.readyState === 4)
+        {
+            if(csvFile.status === 200 || csvFile.status == 0)
+            {
+                text = csvFile.responseText;
+                textArray = text.split("\n");
+                for(i = 0; i < textArray.length; i++){
+                    for(j = 0; j < textArray[i].length; i++){
+                        tempArray = textArray[i].split(",");
+                        cityArray[i] = tempArray[0];
+                        var zipcodeLength = tempArray.length - 1;
+                        zipcodeArray[i] = new Array(zipcodeLength);
+                        for(k = 0; k < zipcodeLength; k++){
+                            zipcodeArray[i][k] = tempArray[k+1];
+                            zipcodeArray[i][k] = zipcodeArray[i][k].replace(/[\r\n]/,'');
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    csvFile.send(null);
+    var citySelected = document.getElementById('city');
+    var selectedCity = citySelected.value;
+    var zipCodeoptions = '';
+    var getCity = 0;
+    for(var q = 0; q < cityArray.length; q++){
+        if(selectedCity == cityArray[q])
+            getCity = q;
+    }
+    for(var n = 0; n < zipcodeArray[getCity].length; n++){
+            zipCodeoptions += '<option value="'+zipcodeArray[getCity][n]+'">' + zipcodeArray[getCity][n] +'</option>';
+    }
+    document.getElementById('zipcodes').innerHTML = zipCodeoptions;
+
+}
+
+
 function alertCity(){
-        var textArray = [];
+    var textArray = [];
     var cityArray = [];
     var zipcodeArray = [];
     var tempArray = [];
@@ -62,7 +114,7 @@ function alertCity(){
 }
 
 
-function autoComplete(){
+function autoCompleteCity(){
     var textArray = [];
     var cityArray = [];
     var zipcodeArray = [];
@@ -103,19 +155,7 @@ function autoComplete(){
         options += '<option value="'+cityArray[o]+'">';
     }
     document.getElementById('cities').innerHTML = options;
-    var citySelected = document.getElementById('city');
-    var selectedCity = citySelected.value;
-    var zipCodeoptions = '';
-    var getCity = 0;
-    for(var q = 0; q < cityArray.length; q++){
-        if(selectedCity == cityArray[q])
-            getCity = q;
-    }
-    for(var n = 0; n < zipcodeArray[getCity].length; n++){
-            zipCodeoptions += '<option value="'+zipcodeArray[getCity][n]+'">' + zipcodeArray[getCity][n] +'</option>';
-    }
-    document.getElementById('zipcodes').innerHTML = zipCodeoptions;
-    
+
     
                                     
 }

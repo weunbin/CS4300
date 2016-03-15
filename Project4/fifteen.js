d9e0c9dd97e4becaac36e09a8b9fe2e3c9f7bc01
MyeungOh <myeungoh@uga.edu>
2016-03-15 18:32:20 -0400

    yeah
    mhmm

--- Project4/fifteen.js
+++ Project4/fifteen.js
@@ -1,1 +1,41 @@
+window.onload = getPic;
 
+var img;
+
+var canvas;
+var stage;
+ 
+var pieces;
+var puzzleWidth = 400;
+var puzzleHeight = 400;
+var pieceWidth = 100;
+var pieceHeight = 100;
+var currentPiece;
+var currentDropPiece;
+ 
+var mouse;
+
+function getPic()
+{
+    document.body.style.backgroundImage = "url('background.jpg')";
+}
+
+
+function onPage()
+{
+    img = new Image();
+    img.addEventListener('load', onImage, false);
+    img.src = "background.jpg";
+}
+
+function onImage(e)
+{
+    setCanvas();
+    initPuzzle();
+}
+
+function initPuzzle()
+{
+    pieces = [];
+    
+}

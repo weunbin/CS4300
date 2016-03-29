09e636069e1d37f0252267a7c54adeb8154cb412
seyun316 <seyun316@uga.edu>
2016-03-29 17:34:35 -0400

    Update matches-submit.php

--- Project5/matches-submit.php
+++ Project5/matches-submit.php
@@ -1,12 +1,24 @@
 <?php include 'top.html';?>
-    <p>
-		<strong>Matches for  <?php echo $_POST["name"];?></strong>
-	<p>
 <?php 
 	$singles="singles.txt";
     
     $list = file_get_contents($singles);
-    
+    $arr = preg_split("/\r\n|\n|\r/", $list);
+    $name = $_POST["name"];
+    $person = null;
+    $match;
+    for ($i = 0; $i < count($arr); $i++) {
+        $temp = explode(",", $arr[$i]);
+        if (strcmp($name, $temp[0]) == 0) {
+            $person = $temp;
+        }
+    }
+    if (is_null($person) || strcmp("", $person[0]) == 0) {
+        echo "<p><strong>Error! Invalid data.</strong><p>We're sorry. You submitted invalid
+        user information. Please go back and try again.";
+    } else {
+        echo "<p><strong>Matches for ".$name."</strong><p>";
+    }
  ?>
         
 <?php include 'bottom.html' ?>

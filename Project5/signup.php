3273e2cdc3497b2a413326474fac9c73183493de
zachmeads <zachmeads14@gmail.com>
2016-03-29 12:34:14 -0400

    Update signup.php

--- Project5/signup.php
+++ Project5/signup.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html>
-	<!--
-	This provided file is the front page that links to two of the files
-	you are going to write, signup.php and matches.php.
-	You can modify this file as necessary to move redundant code out to common.php.
-	-->
-	
-	<head>
-		<title>NerdLuv</title>
-		
-		<meta charset="utf-8" />
-		
-		<!-- instructor-provided CSS and JavaScript links; do not modify -->
-		<link href="heart.gif" type="image/gif" rel="shortcut icon" />
-		<link href="nerdluv.css" type="text/css" rel="stylesheet" />
-		
-		<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
-		
-	</head>
+<?php include 'top.html';?>
 	<body>
 	
 	<div id="bannerarea">
@@ -28,21 +9,16 @@
 			<fieldset>
 				<legend>New User Signup:</legend>
 				<p>
-				  
 				<label for="name">Name:</label>
 				<input type="text" name="name" id="name">
 				<p>
-				  
 				<label for="gender">Gender:</label>
 				<input type="radio" name="gender" value="M">Male
 				<input type="radio" name="gender" value="F" checked> Female
 				<p>
-				  
 				<label>Age:</label>
-				<input type="text" name="age" size="6" maxlength="2">
+				<input type="text" name="age" size="6" maxlength="2" id="age">
 				<p>
-				  
-				
 				<label>	Personality type:</label>
 				<input type="text" name = "ptype" size="6">
 				(
@@ -51,7 +27,6 @@
 				</a>
 				)
 				<p>
-				  
 				<label>Favorite OS</label>
 				<select name="os">
 					<option selected>Windows</option>
@@ -59,11 +34,10 @@
 					<option>Linux</option>
 				</select>
 				<p>
-				  
 				<label>Seeking Age:<label>
-				<input type="text" name="seekMin" size="6" placeholder="min" maxlength="2">
+				<input type="text" name="seekMin" size="6" placeholder="min" maxlength="2" id="seekMin">
 				to
-				<input type="text" name="seekMax" size="6" placeholder="max" maxlength="2">
+				<input type="text" name="seekMax" size="6" placeholder="max" maxlength="2" id="seekMax">
 				<p>
 				<input type="submit"
 				
@@ -71,4 +45,4 @@
 		</form>
 	
 	</body>
-</html>
+<?php include 'bottom.html' ?>

91397ea94c29b5865201534ed87641c0670ddc07
seyun316 <seyun316@uga.edu>
2016-03-29 15:52:13 -0400

    Update signup-submit.php

--- Project5/signup-submit.php
+++ Project5/signup-submit.php
@@ -1,6 +1,4 @@
-<?php 
-	include '/top.html';
-?>
+<?php include 'top.html';?>
 	<p>
 		<strong>Thank You!</strong>
 	<p>
@@ -9,9 +7,18 @@
 	Now <a href="matches.php"> log in to see your matches!</a>
 	
 <?php 
-	$singles="/singles.txt";
-	$userData = $_POST["name"] . "," . $_POST["gender"] . "," . $_POST["age"] . "," .$_POST["ptype"] . "," . $_POST["os"] . "," . $_POST["seekMin"] . "," . $_POST["seekMax"]."\n";
+	$singles="singles.txt";
+        
+    $userName = $_POST["name"];
+    $userGender = $_POST["gender"];
+    $userAge = $_POST["age"];
+    $userType = $_POST["ptype"];
+    $userOS = $_POST["os"];
+    $userMin = $_POST["seekMin"];
+    $userMax = $_POST["seekMax"];
+        
+	$userData = $userName . "," . $userGender . "," . $userAge . "," . $userType . "," . $userOS . "," . $userMin . "," . $userMax ."\n";
 	file_put_contents($singles, $userData,FILE_USE_INCLUDE_PATH | FILE_APPEND);
-	
+    
  ?>
-<?php include '/bottom.html'?>
+<?php include 'bottom.html'?>

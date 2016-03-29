7dedfddd968c99ad306938b55d77818af2ce9431
seyun316 <seyun316@uga.edu>
2016-03-29 17:02:27 -0400

    Update matches-submit.php

--- Project5/matches-submit.php
+++ Project5/matches-submit.php
@@ -1,1 +1,12 @@
-
+<?php include 'top.html';?>
+    <p>
+		<strong>Matches for  <?php echo $_POST["name"];?></strong>
+	<p>
+<?php 
+	$singles="singles.txt";
+    
+    $list = file_get_contents($singles);
+    
+ ?>
+        
+<?php include 'bottom.html' ?>

0ef03afed57192f11e9c6c5c4daf2e476dd48cd0
zachmeads <zachmeads14@gmail.com>
2016-03-29 12:35:55 -0400

    Update signup-submit.php

--- Project5/signup-submit.php
+++ Project5/signup-submit.php
@@ -1,21 +1,17 @@
-<html>
-<head>
-		<title>NerdLuv</title>
-		
-		<meta charset="utf-8" />
-		
-		<!-- instructor-provided CSS and JavaScript links; do not modify -->
-		<link href="heart.gif" type="image/gif" rel="shortcut icon" />
-		<link href="nerdluv.css" type="text/css" rel="stylesheet" />
-		
-		<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
-		
-	</head>
-<body>
 <?php 
-	$singles=fopen("singles.txt","a")
-	$userData = $_POST["name"] . ',' . $_POST["gender"] . ',' . $_POST["age"] . ',' .$_POST["ptype"] . ',' . $_POST["os"] . ',' . $_POST["seekMin"] . ',' . $_POST["seekMax"].\n;
-	file_put_contents($singles, $userData, FILE_APPEND)
+	include '/top.html';
+?>
+	<p>
+		<strong>Thank You!</strong>
+	<p>
+	Welcom to NerdLuv, <?php echo $_POST["name"];?>
+	<p>
+	Now <a href="matches.php"> log in to see your matches!</a>
+	
+<?php 
+	$singles="/singles.txt";
+	$userData = $_POST["name"] . "," . $_POST["gender"] . "," . $_POST["age"] . "," .$_POST["ptype"] . "," . $_POST["os"] . "," . $_POST["seekMin"] . "," . $_POST["seekMax"]."\n";
+	file_put_contents($singles, $userData,FILE_USE_INCLUDE_PATH | FILE_APPEND);
+	
  ?>
-</body>
-</html>
+<?php include '/bottom.html'?>

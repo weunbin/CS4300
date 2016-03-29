97d03823e32090b09cbdee82da38a4806bf19c72
zachmeads <zachmeads14@gmail.com>
2016-03-28 23:15:25 -0400

    Update signup-submit.php

--- Project5/signup-submit.php
+++ Project5/signup-submit.php
@@ -1,1 +1,21 @@
-
+<html>
+<head>
+		<title>NerdLuv</title>
+		
+		<meta charset="utf-8" />
+		
+		<!-- instructor-provided CSS and JavaScript links; do not modify -->
+		<link href="heart.gif" type="image/gif" rel="shortcut icon" />
+		<link href="nerdluv.css" type="text/css" rel="stylesheet" />
+		
+		<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
+		
+	</head>
+<body>
+<?php 
+	$singles=fopen("singles.txt","a")
+	$userData = $_POST["name"] . ',' . $_POST["gender"] . ',' . $_POST["age"] . ',' .$_POST["ptype"] . ',' . $_POST["os"] . ',' . $_POST["seekMin"] . ',' . $_POST["seekMax"].\n;
+	file_put_contents($singles, $userData, FILE_APPEND)
+ ?>
+</body>
+</html>

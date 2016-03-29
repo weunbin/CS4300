5bf64dc33fd8e181ac78e1bd3edb8bf103bc6253
seyun316 <seyun316@uga.edu>
2016-03-29 16:47:50 -0400

    Update signup-submit.php

--- Project5/signup-submit.php
+++ Project5/signup-submit.php
@@ -2,23 +2,22 @@
 	<p>
 		<strong>Thank You!</strong>
 	<p>
-	Welcom to NerdLuv, <?php echo $_POST["name"];?>
+	Welcom to NerdLuv, <?php echo $_POST["name"];?>!
 	<p>
 	Now <a href="matches.php"> log in to see your matches!</a>
 	
 <?php 
-	$singles="singles.txt";
-        
-    $userName = $_POST["name"];
-    $userGender = $_POST["gender"];
-    $userAge = $_POST["age"];
-    $userType = $_POST["ptype"];
-    $userOS = $_POST["os"];
-    $userMin = $_POST["seekMin"];
-    $userMax = $_POST["seekMax"];
-        
-	$userData = $userName . "," . $userGender . "," . $userAge . "," . $userType . "," . $userOS . "," . $userMin . "," . $userMax ."\n";
-	file_put_contents($singles, $userData,FILE_USE_INCLUDE_PATH | FILE_APPEND);
+	$singles="/singles.txt";
     
+  	$userName = $_POST["name"];
+    	$userGender = $_POST["gender"];
+    	$userAge = $_POST["age"];
+    	$userType = $_POST["ptype"];
+    	$userOS = $_POST["os"];
+    	$userMin = $_POST["seekMin"];
+    	$userMax = $_POST["seekMax"];
+    
+	$userData = "\n" . $userName . "," . $userGender . "," . $userAge . "," . $userType . "," . $userOS . "," . $userMin . "," . $userMax;
+	file_put_contents($singles, $userData,FILE_USE_INCLUDE_PATH | FILE_APPEND);
  ?>
 <?php include 'bottom.html'?>

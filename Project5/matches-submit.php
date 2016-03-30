63f4bfadf0bf98933e04de8769607bb138330077
MyeungOh <myeungoh@uga.edu>
2016-03-30 00:08:36 -0400

    hmm

--- Project5/matches-submit.php
+++ Project5/matches-submit.php
@@ -96,12 +96,12 @@
                             $temp = explode(",", $match4[$i]);
                             echo "<div>";
                             echo "<div style=\"float: left;\"><img src=\"user.jpg\" alt=\"user picture\" /></div>";
-                            echo "<div style=\"float: left;\"><fieldset>".$temp[0]."</fieldset><p>";
+                            echo "<div style=\"float: left;\"><fieldset>".$temp[0]."<p>";
                             echo "<ul>";
                             echo "<strong>gender:</strong><li>".$temp[1]."</li>";
                             echo "<strong>age:</strong><li>".$temp[2]."</li>";
                             echo "<strong>type:</strong><li>".$temp[3]."</li>";
-                            echo "<strong>OS:</strong><li>".$temp[4]."</li></div></div></ul>";
+                            echo "<strong>OS:</strong><li>".$temp[4]."</li></div></div></ul></fieldset>";
                             
                         }
                     }

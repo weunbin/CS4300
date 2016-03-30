f98f823617d856411ee9b1bfc9795ece60f79b04
MyeungOh <myeungoh@uga.edu>
2016-03-29 23:36:17 -0400

    mhmm

--- Project5/signup.php
+++ Project5/signup.php
@@ -3,37 +3,41 @@
 		<form method="post" action="signup-submit.php">
 			<fieldset>
 				<legend>New User Signup:</legend>
-				<p>
+				<ul>
+                <p>
                     <label for="name"><strong>Name:</strong></label>
-				    <input type="text" name="name" id="name" maxlength="16">
-				<p>
+                    <li><input type="text" name="name" id="name" maxlength="16"></li>
+                <p>
                     <label for="gender"><strong>Gender:</strong></label>
-				    <input type="radio" name="gender" value="M">Male
-				    <input type="radio" name="gender" value="F" checked> Female
+				    <li><input type="radio" name="gender" value="M">Male
+                    <input type="radio" name="gender" value="F" checked> Female</li>
 				<p>
                     <label><strong>Age:</strong></label>
-				    <input type="text" name="age" size="6" maxlength="2" id="age">
+                    <li><input type="text" name="age" size="6" maxlength="2" id="age"></li>
 				<p>
                     <label><strong>Personality type:</strong></label>
-				    <input type="text" name = "ptype" size="6" pattern="[ieIE]{1}[snSN]{1}[ftFT]{1}[jpJP]{1}">
+				    <li><input type="text" name = "ptype" size="6" pattern="[ieIE]{1}[snSN]{1}[ftFT]{1}[jpJP]{1}">
 				    (
 				    <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">
 				    Don't know your type?	
 				    </a>
-				    )
+                    )</li>
 				<p>
                     <label><strong>Favorite OS</strong></label>
-				    <select name="os">
+				    <li><select name="os">
                         <option selected>Windows</option>
                         <option>Mac OS X</option>
                         <option>Linux</option>
 				    </select>
+                    </li>
 				<p>
                     <label><strong>Seeking Age:</strong></label>
-				    <input type="text" name="seekMin" size="6" placeholder="min" maxlength="2" id="seekMin">
+				    <li><input type="text" name="seekMin" size="6" placeholder="min" maxlength="2" id="seekMin">
 				    to
 				    <input type="text" name="seekMax" size="6" placeholder="max" maxlength="2" id="seekMax">
+                    </li>
 				<p>
+                </ul>
 				<input type="submit" value="Sign Up">
 			</fieldset>
 		</form>

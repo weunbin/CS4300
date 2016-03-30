1b71326762a67425ea87664c1687bcd82fdd3cd6
MyeungOh <myeungoh@uga.edu>
2016-03-29 23:14:19 -0400

    mhm

--- Project5/signup.php
+++ Project5/signup.php
@@ -4,35 +4,35 @@
 			<fieldset>
 				<legend>New User Signup:</legend>
 				<p>
-				<label for="name">Name:</label>
-				<input type="text" name="name" id="name" maxlength="16">
+                    <label for="name"><strong>Name:</strong></label>
+				    <input type="text" name="name" id="name" maxlength="16">
 				<p>
-				<label for="gender">Gender:</label>
-				<input type="radio" name="gender" value="M">Male
-				<input type="radio" name="gender" value="F" checked> Female
+                    <label for="gender"><strong>Gender:</strong></label>
+				    <input type="radio" name="gender" value="M">Male
+				    <input type="radio" name="gender" value="F" checked> Female
 				<p>
-				<label>Age:</label>
-				<input type="text" name="age" size="6" maxlength="2" id="age">
+                    <label><strong>Age:</strong></label>
+				    <input type="text" name="age" size="6" maxlength="2" id="age">
 				<p>
-				<label>	Personality type:</label>
-				<input type="text" name = "ptype" size="6" pattern="[ieIE]{1}[snSN]{1}[ftFT]{1}[jpJP]{1}">
-				(
-				<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">
-				Don't know your type?	
-				</a>
-				)
+                    <label><strong>Personality type:</strong></label>
+				    <input type="text" name = "ptype" size="6" pattern="[ieIE]{1}[snSN]{1}[ftFT]{1}[jpJP]{1}">
+				    (
+				    <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">
+				    Don't know your type?	
+				    </a>
+				    )
 				<p>
-				<label>Favorite OS</label>
-				<select name="os">
-					<option selected>Windows</option>
-					<option>Mac OS X</option>
-					<option>Linux</option>
-				</select>
+                    <label><strong>Favorite OS</strong></label>
+				    <select name="os">
+                        <option selected>Windows</option>
+                        <option>Mac OS X</option>
+                        <option>Linux</option>
+				    </select>
 				<p>
-                    <label>Seeking Age:</label>
-				<input type="text" name="seekMin" size="6" placeholder="min" maxlength="2" id="seekMin">
-				to
-				<input type="text" name="seekMax" size="6" placeholder="max" maxlength="2" id="seekMax">
+                    <label><strong>Seeking Age:</strong></label>
+				    <input type="text" name="seekMin" size="6" placeholder="min" maxlength="2" id="seekMin">
+				    to
+				    <input type="text" name="seekMax" size="6" placeholder="max" maxlength="2" id="seekMax">
 				<p>
 				<input type="submit" value="Sign Up">
 			</fieldset>

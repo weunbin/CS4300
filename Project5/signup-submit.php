6825cff2e97729138a374ca761905a76a3621f5f
seyun316 <seyun316@uga.edu>
2016-03-30 00:14:41 -0400

    Update signup-submit.php

--- Project5/signup-submit.php
+++ Project5/signup-submit.php
@@ -1,14 +1,6 @@
 <?php include 'top.html';?>
-	<p>
-		<strong>Thank You!</strong>
-	<p>
-	Welcom to NerdLuv, <?php echo $_POST["name"];?>!
-	<p>
-	Now <a href="matches.php"> log in to see your matches!</a>
-	
 <?php 
 	$singles="singles.txt";
-    
     	$userName = $_POST["name"];
     	$userGender = $_POST["gender"];
     	$userAge = $_POST["age"];
@@ -16,8 +8,46 @@
     	$userOS = $_POST["os"];
     	$userMin = $_POST["seekMin"];
     	$userMax = $_POST["seekMax"];
-    
-	$userData = "\n" . $userName . "," . $userGender . "," . $userAge . "," . $userType . "," . $userOS . "," . $userMin . "," . $userMax;
-	file_put_contents($singles, $userData,FILE_USE_INCLUDE_PATH | FILE_APPEND);
- ?>
+   	if (!ctype_alpha($userName) || !ctype_digit($userAge) || !ctype_digit($userMin) || !ctype_digit($userMax) || strlen($userType) != 4) {
+        	echo "<p><strong>Error! Invalid data.</strong><p>We're sorry. You submitted invalid
+        	user information. Please go back and try again.";
+    	} else {
+	        $number = 0;
+	        for ($i = 0; $i < 4; $i++) {
+	         	if (strcmp($userType[$i], "I") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "E") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "N") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "F") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "P") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "S") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "T") == 0) {
+	                	$number++;
+	            	}
+	            	if (strcmp($userType[$i], "J") == 0) {
+	                	$number++;
+	            	}
+	        }
+	        if ($number == 4) {
+	            	$userData = "\n" . $userName . "," . $userGender . "," . $userAge . "," . $userType . "," . $userOS . "," . $userMin . "," . $userMax;
+	            	file_put_contents($singles, $userData,FILE_USE_INCLUDE_PATH | FILE_APPEND);
+	        
+	            	echo "<p><strong>Thank You!</strong><p>Welcome to NerdLuv, ".$userName."!<p>Now <a href=\"matches.php\"> log in to see your matches!</a>";
+	        } else {
+	            	echo "<p><strong>Error! Invalid data.</strong><p>We're sorry. You submitted invalid
+	            	user information. Please go back and try again.";
+	        }
+    	}
+?>
 <?php include 'bottom.html'?>

8c20ec404de384deb258741ae3c7758f28c473bd
seyun316 <seyun316@uga.edu>
2016-04-12 06:52:43 -0400

    Update index.php

--- Project6/index.php
+++ Project6/index.php
@@ -14,7 +14,6 @@
 				<a href="index.php"><img src="mymdb.png" alt="banner logo" /></a>
 				My Movie Database
 			</div>
-
 			<div id="main">
 				<h1>The One Degree of Kevin Bacon</h1>
 				<p>Type in an actor's name to see if he/she was ever in a movie with Kevin Bacon!</p>
@@ -44,7 +43,9 @@
 					</fieldset>
 				</form>
 			</div> <!-- end of #main div -->
-		
+		<div id="closer">
+            <a href="index.php"><img src="mymdb.png" alt="banner logo" /></a>
+			</div>
 		</div> <!-- end of #frame div -->
 	</body>
 </html>

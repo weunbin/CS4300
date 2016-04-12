262d5ff48eb7abae9d6702602acb62cfdff562d9
seyun316 <seyun316@uga.edu>
2016-04-12 14:40:17 -0400

    FINISHED

--- Project6/search-all.php
+++ Project6/search-all.php
@@ -37,26 +37,102 @@
                         $actor_id = $id['id'];
                     
                     if(isset($actor_id)){
-                    $sql = $conn->query("SELECT movie.year, movie.name FROM movies movie JOIN roles role ON role.movie_id = movie.id JOIN actors actor ON role.actor_id = actor.id WHERE role.actor_id = '".$actor_id."' ORDER BY movie.year DESC, movie.name ASC");
-                   
-                    $row_num = 0;
-                    
-                    createtablehead();
+                        $sql = $conn->query("SELECT movie.year, movie.name FROM movies movie JOIN roles role ON role.movie_id = movie.id JOIN actors actor ON role.actor_id = actor.id WHERE role.actor_id = '".$actor_id."' ORDER BY movie.year DESC, movie.name ASC");
+
+                        $row_num = 0;
+
+                        createtablehead();
+
+                        foreach($sql as $row){
+                            echo "<tr><td>";
+                            echo $row_num+1;
+                            echo "</td><td>";
+                            echo $row['name'];
+                            echo "</td><td>";
+                            echo $row['year'];
+                            echo "</td></tr>";
+                            $row_num++;
+                        }
+                    } else {   
+                        $weird_name = $conn->query("SELECT id FROM actors a WHERE a.last_name = '".$lname."' AND a.first_name LIKE '".$fname."%'");
                         
-                    foreach($sql as $row){
-                        echo "<tr><td>";
-                        echo $row_num+1;
-                        echo "</td><td>";
-                        echo $row['name'];
-                        echo "</td><td>";
-                        echo $row['year'];
-                        echo "</td></tr>";
-                        $row_num++;
-                    }
-                    }
-                    else{ echo 'Actor ', $fname, ' ', $lname, ' not found.';}
+                        if($weird_name->rowCount() > 0) {
+                            if ($weird_name->rowCount() == 1) {
+                                foreach($weird_name as $id)
+                                    $actor_id = $id['id'];
                     
-                    
+                                if(isset($actor_id)){
+                                    $sql = $conn->query("SELECT movie.year, movie.name FROM movies movie JOIN roles role ON role.movie_id = movie.id JOIN actors actor ON role.actor_id = actor.id WHERE role.actor_id = '".$actor_id."' ORDER BY movie.year DESC, movie.name ASC");
+                                        $row_num = 0;
+
+                                    createtablehead();
+
+                                    foreach($sql as $row){
+                                        echo "<tr><td>";
+                                        echo $row_num+1;
+                                        echo "</td><td>";
+                                        echo $row['name'];
+                                        echo "</td><td>";
+                                        echo $row['year'];
+                                        echo "</td></tr>";
+                                        $row_num++;
+                                    }
+                                }
+                            } else {
+                                $weird_name = $conn->query("SELECT id FROM actors a WHERE a.last_name = '".$lname."' AND a.first_name LIKE '".$fname."' AND a.film_count = (SELECT film_count FROM actors b WHERE b.last_name = '".$lname."' AND b.first_name LIKE '".$fname."' HAVING MAX(film_count))");
+                                if ($weird_name->rowCount() == 1) {
+                                    foreach($weird_name as $id)
+                                        $actor_id = $id['id'];
+
+                                    if(isset($actor_id)){
+                                        $sql = $conn->query("SELECT movie.year, movie.name FROM movies movie JOIN roles role ON role.movie_id = movie.id JOIN actors actor ON role.actor_id = actor.id WHERE role.actor_id = '".$actor_id."' ORDER BY movie.year DESC, movie.name ASC");
+                                            $row_num = 0;
+
+                                        createtablehead();
+
+                                        foreach($sql as $row){
+                                            echo "<tr><td>";
+                                            echo $row_num+1;
+                                            echo "</td><td>";
+                                            echo $row['name'];
+                                            echo "</td><td>";
+                                            echo $row['year'];
+                                            echo "</td></tr>";
+                                            $row_num++;
+                                        }
+                                    }
+                                } else {
+                                    $weird_name = $conn->query("SELECT id FROM actors a WHERE a.last_name = '".$lname."' AND a.first_name LIKE '".$fname."' Having MIN(id)");
+                                    if ($weird_name->rowCount() == 1) {
+                                        foreach($weird_name as $id)
+                                            $actor_id = $id['id'];
+
+                                        if(isset($actor_id)){
+                                            $sql = $conn->query("SELECT movie.year, movie.name FROM movies movie JOIN roles role ON role.movie_id = movie.id JOIN actors actor ON role.actor_id = actor.id WHERE role.actor_id = '".$actor_id."' ORDER BY movie.year DESC, movie.name ASC");
+                                                $row_num = 0;
+
+                                            createtablehead();
+
+                                            foreach($sql as $row){
+                                                echo "<tr><td>";
+                                                echo $row_num+1;
+                                                echo "</td><td>";
+                                                echo $row['name'];
+                                                echo "</td><td>";
+                                                echo $row['year'];
+                                                echo "</td></tr>";
+                                                $row_num++;
+                                            }
+                                        }
+                                    } else {
+                                        echo 'Actor ', $fname, ' ', $lname, ' not found.';
+                                    }
+                                }
+                            }
+                        } else {
+                            echo 'Actor ', $fname, ' ', $lname, ' not found.';   
+                        }
+                    }
                     ?>
                 </table>
                 

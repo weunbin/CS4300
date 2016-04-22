d636c300ea32804243a065cb6a5ada92d4d72def
zachmeads <zachmeads14@gmail.com>
2016-04-22 19:56:29 -0400

    Create index.php

---
+++ Term/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="term.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <header>
+        <div id=bannerarea>NBA Draft Helper
+            <img src="placeholder1.jpg" id="placeholder" />
+        </div>
+    </header>
+
+
+    <nav>
+        <a href="players.php">Players</a>
+        <br>
+        <a href="teams.php">Teams</a>
+        <br>
+
+    </nav>
+
+    <section>
+        <h1>Purpose</h1>
+        <p>To help interested parties search for the best players in the positions they need for their fantasy team.</p>
+        <p>We will pull information from the database:nba which I'm working on converting from a csv file if you guys find a better database to use please tell me right now the database is not attached</p>
+    </section>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>

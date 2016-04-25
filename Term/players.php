23dceb8911d2403a91062dd242500b15c7b8bd78
zachmeads <zachmeads14@gmail.com>
2016-04-25 19:53:24 -0400

    Update players.php

--- Term/players.php
+++ Term/players.php
@@ -1,3 +1,19 @@
 <?php include '/header.html' ?>
+    <form action="action_player.php" method="get">
+        <fieldset>
+            <legend>Player</legend>
+            First Name:<input type="text" maxlength="14" size="14"><br><br>
+            Last Name: <input type=text maxlength="14" size="14"><br><br>
+          Position:  <select name="position">
+                <option value="pg">PG</option>
+                <option value="SF">SF</option>
+                <option value="PF">PF</option>
+                <option value="SG">SG</option>
+                <option value="C">C</option>
 
-<?php include '/footer.html' ?>
+            </select>
+            <br>
+            <input type="submit">
+        </fieldset>
+    </form>
+    <?php include '/footer.html' ?>

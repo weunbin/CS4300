73d60207043e117da88e12286ff65e75ca5a2b83
MyeungOh <myeungoh@uga.edu>
2016-04-27 21:09:58 -0400

    kkl;k;l

--- Term/index.php
+++ Term/index.php
@@ -1,36 +1,9 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="term.css" rel="stylesheet" type="text/css">
-</head>
-
-<body>
-
-    <div class="header">
-
-        NBA STATS HELPER
-        <img id="jumpman" src="jumpman-logo-wallpaper-3.jpg">
-    </div>
-
-    <div class="row">
-
-        <div class="col-3 menu">
-            <ul>
-                <li><a href="players.php">Players</a></li>
-                <li><a href="teams.php">Teams</a></li>
-                <li><a href="index.php">Home</a></li>
-            </ul>
-        </div>
+<?php include('header.html'); ?>
 
         <div class="col-9">
             <h1>The Site</h1>
             <p> TODO:</p>
             <p> still looking for a good data set to use and we need to implement forms for searching for players and by team</p>
         </div>
-
-    </div>
-</body>
-
-</html>
+    
+<?php include('footer.html'); ?>
---
+++ Term/teams.php
@@ -0,0 +1,10 @@
+<?php include '/header.html' ?>
+    <form action="action_team.php" method="get">
+        <fieldset>
+            <legend>Team</legend>
+            Team Name:<input type="text" maxlength="14" size="14"><br><br>
+            <br>
+            <input type="submit">
+        </fieldset>
+    </form>
+    <?php include '/footer.html' ?>

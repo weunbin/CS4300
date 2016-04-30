22dfcb7fd1f715778264bab05680654d3c65298e
seyun316 <seyun316@uga.edu>
2016-04-30 18:00:50 -0400

    Update common.php

--- Term/common.php
+++ Term/common.php
@@ -10,23 +10,28 @@
         echo "<tr>";
         echo "<td>Player</td>";
         echo "<td>Team</td>";
-        echo "<td>Games</td>";
+        echo "<td>Age</td>";
+        echo "<td>GP</td>";
+        echo "<td>W</td>";
+        echo "<td>L</td>";
         echo "<td>Min</td>";
-        echo "<td>Pts</td>";
-        echo "<td>Reb</td>";
-        echo "<td>Ast</td>";
-        echo "<td>Stl</td>";
-        echo "<td>Blk</td>";
-        echo "<td>TO</td>";
         echo "<td>FGM</td>";
         echo "<td>FGA</td>";
         echo "<td>FG%</td>";
-        echo "<td>3PTM</td>";
-        echo "<td>3PTA</td>";
-        echo "<td>3PT%</td>";
+        echo "<td>3PM</td>";
+        echo "<td>3PA</td>";
+        echo "<td>3P%</td>";
         echo "<td>FTM</td>";
         echo "<td>FTA</td>";
         echo "<td>FT%</td>";
+        echo "<td>REB</td>";
+        echo "<td>AST</td>";
+        echo "<td>TOV</td>";
+        echo "<td>STL</td>";
+        echo "<td>BLK</td>";
+        echo "<td>PTS</td>";
+        echo "<td>DD2</td>";
+        echo "<td>TD3</td>";
         echo "</tr>";
     }
 ?>

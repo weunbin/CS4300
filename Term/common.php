41b68a9b9679ce848432dd72cbc25092ed3d0199
seyun316 <seyun316@uga.edu>
2016-04-30 02:20:56 -0400

    Update common.php

--- Term/common.php
+++ Term/common.php
@@ -2,10 +2,14 @@
     function getdataconnection(){
         $username ="root";
         $password ="";
-        $conn = new PDO('mysql:host=localhost;dbname=nbadatabase', $username, $password);
+        $conn = new PDO('mysql:host=localhost;dbname=nba', $username, $password);
         return $conn;
     }
-        
-    
 
+    function createtablehead(){
+        echo "<tr>";
+        echo "<td>Player</td>";
+        echo "<td>Team</td>";
+        echo "<td>Pts</td>";
+    }
 ?>

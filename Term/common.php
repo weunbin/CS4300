54331a4354c43cc4ee810bd5b48e0da72c424d54
seyun316 <seyun316@uga.edu>
2016-04-30 17:12:07 -0400

    Update common.php

--- Term/common.php
+++ Term/common.php
@@ -10,6 +10,23 @@
         echo "<tr>";
         echo "<td>Player</td>";
         echo "<td>Team</td>";
+        echo "<td>Games</td>";
+        echo "<td>Min</td>";
         echo "<td>Pts</td>";
+        echo "<td>Reb</td>";
+        echo "<td>Ast</td>";
+        echo "<td>Stl</td>";
+        echo "<td>Blk</td>";
+        echo "<td>TO</td>";
+        echo "<td>FGM</td>";
+        echo "<td>FGA</td>";
+        echo "<td>FG%</td>";
+        echo "<td>3PTM</td>";
+        echo "<td>3PTA</td>";
+        echo "<td>3PT%</td>";
+        echo "<td>FTM</td>";
+        echo "<td>FTA</td>";
+        echo "<td>FT%</td>";
+        echo "</tr>";
     }
 ?>

c5fdc646c736d22b3c12e2a4705bf197c76ec2ec
seyun316 <seyun316@uga.edu>
2016-04-30 18:00:34 -0400

    Update players.php

--- Term/players.php
+++ Term/players.php
@@ -6,28 +6,22 @@
                     include 'common.php';
                     $conn = getdataconnection();
                     createtablehead();
-                    $sql = $conn->query("SELECT playerstats.Player,playerstats.Team,playerstats.GP,playerstats.MIN,playerstats.PTS,playerstats.REB,playerstats.AST,playerstats.STL,playerstats.BLK,playerstats.TOV,playerstats.FGM,playerstats.FGA,playerstats.`FG%`,playerstats.3PM,playerstats.3PA,playerstats.`3P%`,playerstats.FTM,playerstats.FTA,playerstats.`FT%` FROM playerstats ORDER BY playerstats.PTS DESC");
+                    $sql = $conn->query("SELECT playerstats.Player,playerstats.Team,playerstats.Age,playerstats.GP,playerstats.W,playerstats.L,playerstats.MIN,playerstats.FGM,playerstats.FGA,playerstats.`FG%`,playerstats.3PM,playerstats.3PA,playerstats.`3P%`,playerstats.FTM,playerstats.FTA,playerstats.`FT%`,playerstats.REB,playerstats.AST,playerstats.TOV,playerstats.STL,playerstats.BLK,playerstats.PTS,playerstats.DD2,playerstats.TD3 FROM playerstats ORDER BY playerstats.Pts DESC");
                     foreach($sql as $row){
                         echo "<tr><td>";
                         echo $row['Player'];
                         echo "</td><td>";
                         echo $row['Team'];
                         echo "</td><td>";
+                        echo $row['Age'];
+                        echo "</td><td>";
                         echo $row['GP'];
                         echo "</td><td>";
+                        echo $row['W'];
+                        echo "</td><td>";
+                        echo $row['L'];
+                        echo "</td><td>";
                         echo $row['MIN'];
-                        echo "</td><td>";
-                        echo $row['PTS'];
-                        echo "</td><td>";
-                        echo $row['REB'];
-                        echo "</td><td>";
-                        echo $row['AST'];
-                        echo "</td><td>";
-                        echo $row['STL'];
-                        echo "</td><td>";
-                        echo $row['BLK'];
-                        echo "</td><td>";
-                        echo $row['TOV'];
                         echo "</td><td>";
                         echo $row['FGM'];
                         echo "</td><td>";
@@ -46,6 +40,22 @@
                         echo $row['FTA'];
                         echo "</td><td>";
                         echo $row['FT%'];
+                        echo "</td><td>";
+                        echo $row['REB'];
+                        echo "</td><td>";
+                        echo $row['AST'];
+                        echo "</td><td>";
+                        echo $row['TOV'];
+                        echo "</td><td>";
+                        echo $row['STL'];
+                        echo "</td><td>";
+                        echo $row['BLK'];
+                        echo "</td><td>";
+                        echo $row['PTS'];
+                        echo "</td><td>";
+                        echo $row['DD2'];
+                        echo "</td><td>";
+                        echo $row['TD3'];
                         echo "</td></tr>";
                     }
                 ?>

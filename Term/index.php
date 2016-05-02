e137efdb43a3637099cb8274c23d59c56cc75df5
zachmeads <zachmeads14@gmail.com>
2016-05-01 22:44:17 -0400

    Update index.php

--- Term/index.php
+++ Term/index.php
@@ -1,9 +1,39 @@
 <?php include('header.html'); ?>
+    <div class="row">
+        <div class="col-12 index">
+            <h1>Player Comparrison</h1>
 
-        <div class="col-9">
-            <h1>The Site</h1>
-            <p> TODO:</p>
-            <p> still looking for a good data set to use and we need to implement forms for searching for players and by team</p>
+            <p> The Player Comparrison feature is a tool that allows users to compare players head to head and get a better feel for the players they want. Going to add a lot more here in the morning when I can look at the comparrison page.</p>
         </div>
-    
-<?php include('footer.html'); ?>
+    </div>
+    <div class="row">
+        <div class="col-5 index">
+            <h1>Players</h1>
+            <p>
+                This page contains a sortable table of all current NBA players and their stats. If you are looking for players that excel in one particular area of their game this page will prove beneficial.
+
+            </p>
+        </div>
+        <div class="col2">
+        </div>
+        <div class="col-6 index">
+            <h1>Teams</h1>
+            <p>On the team page click an icon and have that teams statistics displayed in the table along with all the individual stats for each player</p>
+            <p></p>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8 index news">
+            <div id="body">
+                <div id="feedControl">Loading...</div>
+            </div>
+        </div>
+
+        <div class="col-3 index">
+            <a class="embedly-card" href="http://espn.go.com/nba/scoreboard">NBA Basketball Scores - NBA Scoreboard - ESPN.com.</a>
+            <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
+
+        </div>
+    </div>
+    <?php include('footer.html'); ?>

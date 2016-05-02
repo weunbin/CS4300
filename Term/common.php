e23ac58a50e9154bfba2c94eb720e6f7bca0bcda
seyun316 <seyun316@uga.edu>
2016-05-01 23:55:17 -0400

    Update common.php

--- Term/common.php
+++ Term/common.php
@@ -6,35 +6,6 @@
         return $conn;
     }
 
-    function createtablehead(){
-        echo "<tr>";
-        echo "<td>Player</td>";
-        echo "<td>Team</td>";
-        echo "<td>Age</td>";
-        echo "<td>GP</td>";
-        echo "<td>W</td>";
-        echo "<td>L</td>";
-        echo "<td>Min</td>";
-        echo "<td>FGM</td>";
-        echo "<td>FGA</td>";
-        echo "<td>FG%</td>";
-        echo "<td>3PM</td>";
-        echo "<td>3PA</td>";
-        echo "<td>3P%</td>";
-        echo "<td>FTM</td>";
-        echo "<td>FTA</td>";
-        echo "<td>FT%</td>";
-        echo "<td>REB</td>";
-        echo "<td>AST</td>";
-        echo "<td>TOV</td>";
-        echo "<td>STL</td>";
-        echo "<td>BLK</td>";
-        echo "<td>PTS</td>";
-        echo "<td>DD2</td>";
-        echo "<td>TD3</td>";
-        echo "</tr>";
-    }
-
     function createtableheadteam(){
         echo "<tr>";
         echo "<td>Team</td>";
